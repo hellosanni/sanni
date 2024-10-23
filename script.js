091e8619259a1317002e9ab409e863e565c3043d
hiroya-sanni <hiroya.sanni@gmail.com>
2024-10-23 14:06:23 +0900

    added assets

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-window.addEventListener('load', () => {
-  const video = document.getElementById('bg-video');
-  
-    // Ensure video starts as soon as the page is loaded
-    video.play().catch((error) => {
-      console.error("Error playing video automatically:", error);
-    });
-    
-    video.addEventListener('ended', function() {
-      console.log("The video has finished playing.");
-      video.classList.add("fade-out")
-    });
-});
+function handleSunRayAnimationEnd() {
+  const loaderContent = document.getElementById("loader-content-container");
+  loaderContent.classList.add("move-up")
+}
+
+
+setTimeout(() => {
+  handleSunRayAnimationEnd();
+}, 3000)
